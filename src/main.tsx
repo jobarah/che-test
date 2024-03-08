ef0cea526b4d5a7f6609590b5dcc44a9c00b9e3c
jenamorado <jobarah01@gmail.com>
2024-03-08 12:40:25 -0600

    updated id mounting point

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ console.log('[ERWT] : Renderer execution started');
   // Application to Render
   const app = <Application />;
   // Render application in DOM
-  createRoot(document.getElementById('app')).render(app);
+  createRoot(document.getElementById('che-mounting-point')).render(app);
 
 export const init = (clientId: string) => {
   console.log('clientId', clientId);
