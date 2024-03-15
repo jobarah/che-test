77a5ebed939546543b4ce0bece4230cef8c950fe
jenamorado <jobarah01@gmail.com>
2024-03-15 12:31:15 -0600

    added markdown parser

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Message.css'
+import { marked } from 'marked'
 
 interface MessageProps {
   message: string;
@@ -7,10 +8,23 @@ interface MessageProps {
 }
 
 function Message(props: MessageProps) {
+  const [parsedMessage, setParsedMessage] = useState('');
+
+  useEffect(() => {
+    async function parseMessage() {
+      const res = await marked.parse(props.message)
+
+      console.log(res);
+
+      setParsedMessage(res);
+    }
+
+    parseMessage();
+  }, [props.message])
+
   return (
     <div className="bubble">
-      <p className={`message ${props.sender === 'client' ? 'left' : 'right' }`}>
-        {props.message}
+      <p className={`message ${props.sender === 'client' ? 'left' : 'right' }`} dangerouslySetInnerHTML={{__html: parsedMessage}}>
       </p>
     </div>
   );
